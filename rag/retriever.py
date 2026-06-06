47803b7e4173b6f084027fe9f8aed38cd3f33e42
Pankaj kumar sahoo <pankajsahoo8260@gmail.com>
2026-06-06 11:43:46 +0530

    Update retriever.py

--- rag/retriever.py
+++ rag/retriever.py
@@ -1,7 +1,74 @@
+# from langchain_community.vectorstores import Chroma
+# from langchain_huggingface import HuggingFaceEmbeddings
+
+# CHROMA_DB_DIR = "chroma_db"
+
+# embeddings = HuggingFaceEmbeddings(
+#     model_name="sentence-transformers/all-MiniLM-L6-v2"
+# )
+
+# vectordb = Chroma(
+#     persist_directory=CHROMA_DB_DIR,
+#     embedding_function=embeddings
+# )
+
+# retriever = vectordb.as_retriever(
+#     search_type="mmr",
+#     search_kwargs={
+#         "k": 10,
+#         "fetch_k": 20
+#     }
+# )
+
+# def search_documents(query):
+
+#     docs = retriever.invoke(query)
+
+#     print("\nRESULTS:\n")
+
+#     for i, doc in enumerate(docs, 1):
+
+#         print("=" * 60)
+#         print(f"Result {i}")
+
+#         print("\nTitle:")
+#         print(doc.metadata.get("title", ""))
+
+#         print("\nURL:")
+#         print(doc.metadata.get("url", ""))
+
+#         print("\nContent:")
+#         print(doc.page_content[:500])
+
+# if __name__ == "__main__":
+
+#     while True:
+
+#         q = input("\nAsk a question: ")
+
+#         if q.lower() == "exit":
+#             break
+
+#         search_documents(q)
+
+
+
+
+
+#--------------------------------------------------------
+import os
+
 from langchain_community.vectorstores import Chroma
 from langchain_huggingface import HuggingFaceEmbeddings
 
+from rag.vector_db import create_vector_db
+
 CHROMA_DB_DIR = "chroma_db"
+
+# Create DB if missing
+if not os.path.exists(CHROMA_DB_DIR):
+    print("Creating Vector Database...")
+    create_vector_db()
 
 embeddings = HuggingFaceEmbeddings(
     model_name="sentence-transformers/all-MiniLM-L6-v2"
@@ -19,34 +86,3 @@ retriever = vectordb.as_retriever(
         "fetch_k": 20
     }
 )
-
-def search_documents(query):
-
-    docs = retriever.invoke(query)
-
-    print("\nRESULTS:\n")
-
-    for i, doc in enumerate(docs, 1):
-
-        print("=" * 60)
-        print(f"Result {i}")
-
-        print("\nTitle:")
-        print(doc.metadata.get("title", ""))
-
-        print("\nURL:")
-        print(doc.metadata.get("url", ""))
-
-        print("\nContent:")
-        print(doc.page_content[:500])
-
-if __name__ == "__main__":
-
-    while True:
-
-        q = input("\nAsk a question: ")
-
-        if q.lower() == "exit":
-            break
-
-        search_documents(q)
